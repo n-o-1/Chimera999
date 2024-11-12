1b4bbf4dc653e9c08e7774f65ad6f2aa24d7052b
N01 <108454014+I-N01-I@users.noreply.github.com>
2024-11-12 15:00:32 -0600

    Add files via upload

---
+++ version.h
@@ -0,0 +1,12 @@
+#include "math/data_types.h"
+
+#define CHIMERA_AUTHOR "Kavawuvi, gbMichelle, Devieth, N01"
+#define CHIMERA_BUILD_NUMBER 1
+#define CHIMERA_ALPHA_VERSION -999
+#define CHIMERA_LUA_INTERPRETER 2.05
+
+#ifdef CHIMERA_ALPHA_VERSION
+#define CHIMERA_BUILD_STRING STR(CHIMERA_ALPHA_VERSION) " (" STR(CHIMERA_BUILD_NUMBER) ") alpha"
+#else
+#define CHIMERA_BUILD_STRING STR(CHIMERA_BUILD_NUMBER)
+#endif
