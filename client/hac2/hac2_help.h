9376bb6476c0d86f1c577803ba16444aae03b707
RNDM-N01 <108454014+I-N01-I@users.noreply.github.com>
2025-02-24 17:33:25 -0600

    Create hac2_help.h

---
+++ client/hac2/hac2_help.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "../command/command.h"
+ChimeraCommandError hac2_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_bookmarking_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_binds_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_copy_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_optic_commands(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_timer_commands(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_chat_commands(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_fov_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_widescreen_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_queue_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_devcam_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_draw_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_recalculate_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_objects_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_cache_command(size_t argc, const char **argv) noexcept;
+ChimeraCommandError hac2_meme_command(size_t argc, const char **argv) noexcept;
