8cacdcca4d4cbadb9ae2607d3cf87ba3a88a0411
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:12:30 -0500

    Update open_sauce.cpp

--- client/open_sauce.cpp
+++ client/open_sauce.cpp
@@ -4,7 +4,8 @@
 static bool check_dinput8() noexcept {
     auto *module = GetModuleHandle("dinput8.dll");
     if(!module) return false;
-    for(int i=0;i<0x2000;i++) {
+    for(int i=0;i<0x2000;i++)
+    {
         if(memcmp(reinterpret_cast<char *>(module) + i, ".yelo", 5) == 0) return true;
     }
     return false;
