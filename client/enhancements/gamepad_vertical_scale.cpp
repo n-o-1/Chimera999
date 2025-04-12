48c0d639879f79e3e28473a0d5c2dcfc7c122c95
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:14:28 -0500

    Update gamepad_vertical_scale.cpp

--- client/enhancements/gamepad_vertical_scale.cpp
+++ client/enhancements/gamepad_vertical_scale.cpp
@@ -5,14 +5,18 @@
 
 ChimeraCommandError gamepad_vertical_scale_command(size_t argc, const char **argv) noexcept {
     static float value = 0;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         auto &vertical_1_sig = get_signature("vertical_1_sig");
         auto &vertical_2_sig = get_signature("vertical_2_sig");
         auto new_value = strtof(argv[0], nullptr);
         if(new_value == 1) new_value = 0;
-        if((new_value == 0) != (value == 0)) {
-            if(new_value != 0) {
-                static unsigned char osig[] = {
+        if((new_value == 0) != (value == 0))
+        {
+            if(new_value != 0)
+            {
+                static unsigned char osig[] =
+                {
                     // fld dword ptr [ebx*4+addr]
                     0xD9, 0x04, 0x9D, 0xFF, 0xFF, 0xFF, 0xFF,
 
@@ -41,7 +45,8 @@ ChimeraCommandError gamepad_vertical_scale_command(size_t argc, const char **arg
                 write_code_any_value(vertical_2_sig.address() + 1, reinterpret_cast<int>(b.data) - reinterpret_cast<int>(vertical_2_sig.address() + 5));
                 write_code_any_value(b.data + 7 + 6 + 1, reinterpret_cast<int>(vertical_2_sig.address() + 5) - reinterpret_cast<int>(b.data + 7 + 6 + 5));
             }
-            else {
+            else
+            {
                 vertical_1_sig.undo();
                 vertical_2_sig.undo();
             }
