f25a2c55ad0e9ad9338b26240cbeef056efe7830
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:12:23 -0500

    Update anisotropic_filtering.cpp

--- client/visuals/anisotropic_filtering.cpp
+++ client/visuals/anisotropic_filtering.cpp
@@ -5,11 +5,14 @@
 
 ChimeraCommandError af_command(size_t argc, const char **argv) noexcept {
     static bool active = false;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         auto new_value = bool_value(argv[0]);
-        if(new_value != active) {
+        if(new_value != active)
+        {
             auto &setting = **reinterpret_cast<char **>(get_signature("af_is_enabled_sig").address() + 1);
-            if(new_value && setting) {
+            if(new_value && setting)
+            {
                 console_out_warning("Disable buffering is already enabled (likely via config.txt)!");
             }
             setting = new_value;
