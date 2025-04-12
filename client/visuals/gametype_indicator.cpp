8766ffa53d6ad184d48dfb78f9097117ca2f2510
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:12:21 -0500

    Update gametype_indicator.cpp

--- client/visuals/gametype_indicator.cpp
+++ client/visuals/gametype_indicator.cpp
@@ -5,14 +5,18 @@
 
 ChimeraCommandError block_gametype_indicator_command(size_t argc, const char **argv) noexcept {
     static auto active = false;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         bool new_value = bool_value(argv[0]);
-        if(new_value != active) {
+        if(new_value != active)
+        {
             auto *team_icon_background_name = *reinterpret_cast<unsigned char **>(get_signature("team_icon_background_name_sig").address() + 1);
-            if(new_value) {
+            if(new_value)
+            {
                 write_code_any_value(team_icon_background_name, 'x');
             }
-            else {
+            else
+            {
                 write_code_any_value(team_icon_background_name, 'u');
             }
             active = new_value;
