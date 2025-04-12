8201def5d0d1eb0eff2369313c4ef0eac6a4d242
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:14:09 -0500

    Update throttle_fps.cpp

--- client/enhancements/throttle_fps.cpp
+++ client/enhancements/throttle_fps.cpp
@@ -8,11 +8,13 @@ static double throttle_fps = 0.0;
 
 static void on_frame() noexcept {
     auto seconds_per_frame = 1.0 / throttle_fps;
-    do {
+    do
+    {
         LARGE_INTEGER now_time;
         QueryPerformanceCounter(&now_time);
         auto r = counter_time_elapsed(prev_time, now_time);
-        if(r >= seconds_per_frame) {
+        if(r >= seconds_per_frame)
+        {
             prev_time = now_time;
             break;
         }
@@ -26,29 +28,36 @@ static void on_frame() noexcept {
 ChimeraCommandError throttle_fps_command(size_t argc, const char **argv) noexcept {
     if(argc == 1) {
         auto new_value = atof(argv[0]);
-        if(new_value != throttle_fps) {
-            if(new_value <= 5.0) {
-                if(new_value != 0.0) {
+        if(new_value != throttle_fps)
+        {
+            if(new_value <= 5.0)
+            {
+                if(new_value != 0.0)
+                {
                     console_out_error("Invalid framerate.");
                     return CHIMERA_COMMAND_ERROR_FAILURE;
                 }
                 new_value = 0.0;
             }
 
-            if(new_value == 0.0) {
+            if(new_value == 0.0)
+            {
                 remove_frame_event(on_frame);
             }
-            else {
+            else
+            {
                 add_frame_event(on_frame, EVENT_PRIORITY_FINAL);
                 QueryPerformanceCounter(&prev_time);
             }
             throttle_fps = new_value;
         }
     }
-    if(throttle_fps == 0.0) {
+    if(throttle_fps == 0.0)
+    {
         console_out("off");
     }
-    else {
+    else
+    {
         console_out(std::to_string(throttle_fps));
     }
     return CHIMERA_COMMAND_ERROR_SUCCESS;
