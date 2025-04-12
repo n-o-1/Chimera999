08e8228ea99319b3c82b9f1cffed8fca982d94c0
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:14:44 -0500

    Update devmode.cpp

--- client/debug/devmode.cpp
+++ client/debug/devmode.cpp
@@ -5,15 +5,19 @@
 
 ChimeraCommandError devmode_command(size_t argc, const char **argv) noexcept {
     static auto enabled = false;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         bool new_value = bool_value(argv[0]);
-        if(new_value != enabled) {
+        if(new_value != enabled)
+        {
             auto &devmode_sig = get_signature("devmode_sig");
-            if(new_value) {
+            if(new_value)
+            {
                 const unsigned char new_v[] = { 0xB1, 0x01, 0x90, 0x90, 0x90, 0x90 };
                 write_code_c(devmode_sig.address(), new_v);
             }
-            else {
+            else
+            {
                 devmode_sig.undo();
             }
             enabled = new_value;
