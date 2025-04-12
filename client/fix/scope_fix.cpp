3b6696f680109eb8513e8be30deb314990837ff8
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:13:40 -0500

    Update scope_fix.cpp

--- client/fix/scope_fix.cpp
+++ client/fix/scope_fix.cpp
@@ -10,7 +10,8 @@ void apply_scope_fix() {
     auto &resolution = get_resolution();
     float ar = static_cast<float>(resolution.width) / resolution.height;
     auto expected = ar * *height;
-    if(expected != *width) {
+    if(expected != *width)
+    {
         DWORD prota;
         DWORD protb;
         VirtualProtect(width, sizeof(*width), PAGE_READWRITE, &prota);
@@ -27,17 +28,22 @@ bool widescreen_scope_mask_active = false;
 
 ChimeraCommandError widescreen_scope_fix_command(size_t argc, const char **argv) noexcept {
     extern int widescreen_fix_active;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         bool new_value = bool_value(argv[0]);
-        if(new_value != widescreen_scope_mask_active) {
-            if(new_value) {
-                if(widescreen_fix_active) {
+        if(new_value != widescreen_scope_mask_active)
+        {
+            if(new_value)
+            {
+                if(widescreen_fix_active)
+                {
                     console_out_error("chimera_widescreen_fix is enabled. Turn this off, first.");
                     return CHIMERA_COMMAND_ERROR_FAILURE;
                 }
                 add_tick_event(apply_scope_fix);
             }
-            else {
+            else
+            {
                 remove_tick_event(apply_scope_fix);
                 undo_scope_fix();
             }
