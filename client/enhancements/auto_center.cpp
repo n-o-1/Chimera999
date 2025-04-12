5a53d579dae2e85ea098c10487dc14ab1a46b15c
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:14:37 -0500

    Update auto_center.cpp

--- client/enhancements/auto_center.cpp
+++ client/enhancements/auto_center.cpp
@@ -8,7 +8,8 @@ const short mod[] = { 0x8B, 0x51, 0x54, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0x90
 
 static size_t x = 0;
 static void auto_center_frame() noexcept {
-    if(x++ == 1) {
+    if(x++ == 1)
+    {
         write_code_s(get_signature("auto_center_sig").address(),mod);
         remove_frame_event(auto_center_frame);
     }
@@ -21,15 +22,18 @@ static void auto_center_tick() noexcept {
 
 ChimeraCommandError auto_center_command(size_t argc, const char **argv) noexcept {
     static int auto_center = 0;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         auto &auto_center_s = get_signature("auto_center_sig");
         auto new_value = atol(argv[0]);
-        if(new_value != auto_center) {
+        if(new_value != auto_center)
+        {
             auto_center_s.undo();
             remove_pretick_event(auto_center_tick);
             remove_frame_event(auto_center_frame);
 
-            switch(new_value) {
+            switch(new_value)
+            {
             case 0:
                 break;
             case 1:
