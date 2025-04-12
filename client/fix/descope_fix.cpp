c5ab5544ff1845ee38e744df932cd6e2f08bdd94
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:13:48 -0500

    Update descope_fix.cpp

--- client/fix/descope_fix.cpp
+++ client/fix/descope_fix.cpp
@@ -13,10 +13,12 @@ static void set_halo_descoping(bool do_it) {
     if(!do_it == descope_fix_enabled) return;
     descope_fix_enabled = !do_it;
     auto &descope_fn_sig = get_signature("descope_fix_sig");
-    if(do_it) {
+    if(do_it)
+    {
         descope_fn_sig.undo();
     }
-    else {
+    else
+    {
         const short mod[] = {0x90,0x90,0x90,0x90,0x90};
         write_code_s(descope_fn_sig.address(), mod);
     }
@@ -28,13 +30,15 @@ static void fix_descoping() noexcept {
     auto id = HaloPlayer().object_id();
     HaloObject object(id);
     auto *data = object.object_data();
-    if(data) {
+    if(data)
+    {
         const auto &object_health = *reinterpret_cast<const float *>(data + 0xE0);
         const auto &object_shield = *reinterpret_cast<const float *>(data + 0xE4);
 
         set_halo_descoping(object_shield > shield && object_shield > 1.0);
 
-        if(object_health < health || (object_shield < shield && (shield - object_shield) > 0.01)) {
+        if(object_health < health || (object_shield < shield && (shield - object_shield) > 0.01))
+        {
           if(descope_fix_enabled) do_descope(id);
         }
 
