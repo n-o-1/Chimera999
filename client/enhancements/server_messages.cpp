0e84f68037dbcbeec5b185c88196793d469b3dcb
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:14:21 -0500

    Update server_messages.cpp

--- client/enhancements/server_messages.cpp
+++ client/enhancements/server_messages.cpp
@@ -12,19 +12,22 @@ static void block_next_message() noexcept {
     write_code_any_value(server_messages_3_sig.address() + 1, reinterpret_cast<int>(unblock_next_message) - reinterpret_cast<int>(server_messages_3_sig.address() + 5));
 }
 
-/// Function for command chimera_block_server_messages
 ChimeraCommandError block_server_messages_command(size_t argc, const char **argv) noexcept {
-    if(argc == 1) {
+    if(argc == 1)
+    {
         bool new_value = bool_value(argv[0]);
-        if(new_value != block_server_messages_active) {
+        if(new_value != block_server_messages_active)
+        {
             auto &server_messages_1_sig = get_signature("server_messages_1_sig");
             auto &server_messages_2_sig = get_signature("server_messages_2_sig");
             auto &server_messages_3_sig = get_signature("server_messages_3_sig");
-            if(new_value) {
+            if(new_value)
+            {
                 const short nope[] = { 0x90, 0x90, 0x90, 0x90, 0x90 };
                 write_code_s(server_messages_1_sig.address(), nope);
 
-                const unsigned char data[] = {
+                const unsigned char data[] =
+                {
                     // jle return
                     0x7E, 0x0A,
 
@@ -49,7 +52,8 @@ ChimeraCommandError block_server_messages_command(size_t argc, const char **argv
                 write_code_any_value(codecave.data + 7 + 1, *reinterpret_cast<int *>(server_messages_2_sig.signature() + 2) + reinterpret_cast<int>(server_messages_2_sig.address() + 6) - reinterpret_cast<int>(codecave.data + 7 + 5));
                 write_code_any_value(codecave.data + 12 + 1, reinterpret_cast<int>(server_messages_2_sig.address() + 5) - reinterpret_cast<int>(codecave.data + 12 + 5));
             }
-            else {
+            else
+            {
                 server_messages_1_sig.undo();
                 server_messages_2_sig.undo();
                 server_messages_3_sig.undo();
