11dbb439c6e5dcd8ee453dd1baca89b2ac78522f
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:14:01 -0500

    Update zoom_blur.cpp

--- client/enhancements/zoom_blur.cpp
+++ client/enhancements/zoom_blur.cpp
@@ -6,7 +6,8 @@
 
 ChimeraCommandError block_zoom_blur_command(size_t argc, const char **argv) noexcept {
     static bool active = false;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         ChimeraSignature &zoom_blur_1_s = get_signature("zoom_blur_1_sig");
         ChimeraSignature &zoom_blur_2_s = get_signature("zoom_blur_2_sig");
         ChimeraSignature &zoom_blur_3_s = get_signature("zoom_blur_3_sig");
@@ -18,14 +19,17 @@ ChimeraCommandError block_zoom_blur_command(size_t argc, const char **argv) noex
         const short zoom_blur_4_mod[] = {-1,   0x01};
 
         bool new_value = bool_value(argv[0]);
-        if(new_value != active) {
-            if(new_value) {
+        if(new_value != active)
+        {
+            if(new_value)
+            {
                 write_code_s(zoom_blur_1_s.address(), zoom_blur_1_mod);
                 write_code_s(zoom_blur_2_s.address(), zoom_blur_2_mod);
                 if(!hac2_present()) write_code_s(zoom_blur_3_s.address(), zoom_blur_3_mod);
                 write_code_s(zoom_blur_4_s.address(), zoom_blur_4_mod);
             }
-            else {
+            else
+            {
                 zoom_blur_1_s.undo();
                 zoom_blur_2_s.undo();
                 if(!hac2_present()) zoom_blur_3_s.undo();
