6de7962d7c99a17ae35d0c0fdb984f4a6d9501f9
RNDM-N01 <108454014+n-o-1@users.noreply.github.com>
2025-04-12 16:14:23 -0500

    Update multitexture_overlay.cpp

--- client/enhancements/multitexture_overlay.cpp
+++ client/enhancements/multitexture_overlay.cpp
@@ -4,15 +4,19 @@
 
 ChimeraCommandError block_mo_command(size_t argc, const char **argv) noexcept {
     static auto active = false;
-    if(argc == 1) {
+    if(argc == 1)
+    {
         bool new_value = bool_value(argv[0]);
-        if(new_value != active) {
+        if(new_value != active)
+        {
             auto &multitexture_overlay_sig = get_signature("multitexture_overlay_sig");
-            if(new_value) {
+            if(new_value)
+            {
                 const short multitexture_overlay_mod[] = {  -1,   -1, 0x60};
                 write_code_s(multitexture_overlay_sig.address(), multitexture_overlay_mod);
             }
-            else {
+            else
+            {
                 multitexture_overlay_sig.undo();
             }
             active = new_value;
